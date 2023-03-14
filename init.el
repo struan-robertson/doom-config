1d15f243fda0933d246e60b5645178a35e881f0c
Struan Robertson <struanrobertson@protonmail.com>
2023-03-14 18:42:35 +0000

    Add dragndrop

--- init.el
+++ init.el
@@ -139,11 +139,11 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +jupyter +roam2)               ; organize your plain life in plain text
+       (org +jupyter +roam2 +dragndrop)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +lsp +pyright +poetry +tree-sitter)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
