56ce0cd0643840c94ad7549da5b80f4ca40787be
Struan Robertson <contact@struanrobertson.co.uk>
2024-01-08 14:36:54 +0000

    Remove jupyter and add present flags to org mode

--- init.el
+++ init.el
@@ -139,7 +139,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +jupyter +roam2 +dragndrop)               ; organize your plain life in plain text
+       (org +roam2 +dragndrop +present)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
