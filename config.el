08557d1bc903197e0f6c6f6b954d1d46d9aa811b
Struan Robertson <contact@struanrobertson.co.uk>
2024-01-22 14:45:03 +0000

    Use "Inter Variable" font on laptop

--- config.el
+++ config.el
@@ -3,8 +3,12 @@
 ;;;;;;;;;;;;;;;; Doom Config ;;;;;;;;;;;;;;;;;;;
 
 ;; Doom fonts
-(setq doom-font (font-spec :family "Fira Code" :size 16)
-      doom-variable-pitch-font (font-spec :family "Inter" :size 18))
+(setq doom-font (font-spec :family "Fira Code" :size 16))
+
+;; Annoyingly Inter is called "Inter Variable" on Ubuntu and "Inter" on Nix
+(if (string= (system-name) "neon")
+        (setq doom-variable-pitch-font (font-spec :family "Inter Variable" :size 18))
+    (setq doom-variable-pitch-font (font-spec :family "Inter" :size 18)))
 
 ;; Use Fish shell
 (setq shell-file-name (executable-find "bash"))
