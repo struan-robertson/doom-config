7d60d94e39cc5935ea69eb29ee96bb1161aa5410
Struan Robertson <contact@struanrobertson.co.uk>
2024-01-22 19:21:23 +0000

    Add jupyter for org babel

--- packages.el
+++ packages.el
@@ -1,15 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; Use jupyter-kernel in Org babel source blocks
-;; (package! jupyter)
-
-;; Unpin org-roam for use with org-roam-ui
-;; (unpin! org-roam)
-
-;; UI for visualising org-roam
-;; (package! org-roam-ui)
-
 ;; Automatic toggling of LaTeX fragments
 (package! org-fragtog)
 
